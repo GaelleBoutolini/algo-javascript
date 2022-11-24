1f9b60acb9ca68f4bb00b5d7cecb4bd97eebe758
Hanaline <gaelle.boutolini.simplon@gmail.com>
2022-11-24 17:25:01 +0100

    git commit -m

---
+++ index.js
@@ -0,0 +1,34 @@
+//document.getElementsByClassName(."title");
+/*const titre = document.querySelector(".title");
+const button = document.querySelector("#btn");
+button.addEventListener('click'), () => {
+    alert(/);
+};*/
+
+/*document.querySelector(".title").style.fontSize="5rem"*/
+
+/******************ALGORYTHM********************** */
+
+//Afficher nom de la console : C'est L'Algo
+
+let firstName; //Créer une variable firstName
+firstName = "Hanna"; //Initialiser une variable
+console.log(firstName);  //Afficher la variable
+
+//Lui allouer une valeur différente
+firstName ="Yvette";
+//Afficher la variable
+console.log(firstName);
+//On déclare une seule fois la variable avec "let"
+//Mais on l'initialise plusieurs fois, autant qu'on veut.
+
+///créer une constante firstName
+//Un "const" ne peut pas être initialisée, c'est-à-dire, qu'elle ne change pas de valeur.
+
+const lastName = "Fidèle";
+//afficher la conctste
+console.log(lastName);
+
+
+//La portée dépend de l'endroit où est déclarée.
+//La boucle est toujours dans une focntion 
